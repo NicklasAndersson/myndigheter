fefa4822a72129fa6bb3fd2edbc88a711b414c25
Nicklas Andersson <nicklas@wwn.se>
2025-01-07 09:53:50 +0100

    med whois

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,51 @@ function App() {
 
   const myndigheter = async () => {
     const resp = await fetch('https://alla-myndigheter.wwn.workers.dev/')
+    const respJson = await resp.json();
+    const respWhois = await Promise.all( respJson.map(async mynd => {
+      try {
+        const url = mynd.Webbadress
+        const parsedUrl = URL.parse(url.startsWith("http") ? url : `https://${url}`)
+        const hostname = parsedUrl.hostname.replace('www.', '').split('.').slice(-2).join('.')
 
-    setMyndighet(await resp.json())
+        const whois = await fetch(`https://http-whois.wwn.workers.dev/?domain=${hostname}`,
+          {
+            headers: {
+              "Accept": "application/json"
+            }
+          }
+        )
+        const whoisJson = await whois.json()
+        return {
+          ...mynd,
+          'whois': whoisJson
+        }
+      } catch {
+        
+      }
+      return {
+        ...mynd
+      }
+    }))
+      setMyndighet(respWhois)
+      console.log(respWhois)
   }
+
+
 
   const paginationModel = { page: 0, pageSize: 50 };
   const columns = [
     { field: 'Namn', headerName: 'Namn', width: 350 },
     { field: 'Organisationsnr', headerName: 'Org nr', width: 110 },
     { field: 'Webbadress', headerName: 'Webbadress', width: 350 },
-    { field: 'Epost', headerName: 'Epost', width: 250 },
+    { field: 'Epost', headerName: 'Epost', width: 200 },
+    { field: 'whois', headerName: 'Förfallodatum', with: 150, 
+      valueGetter: (value) => {
+        if(value && value.expires){
+          return value.expires
+        }
+        return 'okänt'
+    }},
   ];
   
   return (
